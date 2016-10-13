baab711966198fe628a7afc73ffb1569a961910d
marble2388 <jefftheperson1@hotmail.com>
2016-10-12 20:54:11 -0400

    not really sure what i changed

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,13 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Jeff Hall',
-    message: 'Still Sleeping'});
+    message: 'Still hoping that web dev is not the only potential job after this'});
 });
 
 /* GET about me page */
 router.get('/about', function(req, res, next) {
   res.render('about', { title: 'About Me',
-    message: 'Jeff Hall' });
+    message: 'Jeff Hall - thats all you need to know' });
 });
 /* GET projects page. */
 router.get('/project', function(req, res, next) {
@@ -20,7 +20,7 @@ router.get('/project', function(req, res, next) {
 /* GET services page. */
 router.get('/servicePage', function(req, res, next) {
   res.render('servicePage', { title: 'Contact Me',
-    message: 'Prices will vary'});
+    message: 'Prices will vary - NO web dev ever there isnt enough money in the world'});
 });
 /* GET contact page. */
 router.get('/contact', function(req, res, next) {
